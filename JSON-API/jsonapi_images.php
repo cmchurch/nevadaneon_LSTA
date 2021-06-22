bd80119fd705adeb984794d956bced8e20d70187
Chris Church <christopherchurch@unr.edu>
2021-06-22 15:34:33 -0700

    update to fetch image thumbnails for parent items

--- JSON-API/jsonapi_images.php
+++ JSON-API/jsonapi_images.php
@@ -1,22 +1,26 @@
 <?php
 
-
+$output = fopen("images-by-uuid_parent.csv", "w"); 
 $parent_data=json_decode(file_get_contents("nnn.json"));
 
-
-foreach ($parent_data as $datum) {
+fwrite($output,"UUID\tIMAGES\n");
+$total=count($parent_data);
+foreach ($parent_data as $key=>$datum) {
  $media_url="http://special.library.unlv.edu/jsonapi/media/image/?filter[field_media_of.id][value]=".$datum->id;
  $media_json=json_decode(file_get_contents($media_url));
- 
+ fwrite($output,$datum->id."\t");
+ $imagesArray=[];
  foreach ($media_json->data as $media_element) {
 	$uid_file = $media_element->relationships->field_media_image->data->id;
  	$file_url="http://special.library.unlv.edu/jsonapi/file/file/".$uid_file;
  	$file_json=json_decode(file_get_contents($file_url));
-	echo $file_json->data->attributes->uri->url.",";
+	array_push($imagesArray, $file_json->data->attributes->uri->url);
  }
-echo "\n";
+ fwrite($output,implode(",",$imagesArray)."\n");
+ $progress = $key/$total*100;
+  echo "PROGRESS: $progress %     \r";
 }
-
+fclose($output);
 
 
 ?>

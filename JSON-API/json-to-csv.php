8724570ace04222453b1dfdf316d2a7510436873
Chris Church <christopherchurch@unr.edu>
2021-06-29 12:36:57 -0700

    switch to proper arrays for csv and node iteration

--- JSON-API/json-to-csv.php
+++ JSON-API/json-to-csv.php
@@ -33,7 +33,7 @@ function getlatlon($node)
 #***********************************
 
 echo "\n\nTRANSFORMING JSON OBJECT INTO CSV FOR CMS IMPORT\n";
-echo "*************************************************\n\n";
+echo "*************************************************\n\nSTART\n";
 #READ IN THE JSONS FROM CACHE
 $input_path = "JSON-OUTPUT/media-nodes.json";
 $mediaNodes = json_decode(file_get_contents($input_path));
@@ -42,14 +42,14 @@ $dcNodes = json_decode(file_get_contents($input_path));
 $input_path = "JSON-OUTPUT/files.json";
 $fileNodes = json_decode(file_get_contents($input_path));
 
-#open an a file to output as csv
-$output = fopen("CSV-OUTPUT/import.csv", "w");
-
 #set variables
 $counter=1; #so we don't run the entire thing while testing
-#$mediaUse_taxonomy_uuid="7bb44572-2b5c-4c28-b9f3-423c578455a8"; #media use UUID taxonomy term for service files
-$mediaUse_taxonomy_uuid="0db579ad-a810-45bb-acd1-002bf314b50f"; #media use UUID taxonomy term for thumbnail files
+$csvLines =[]; #this is where we will store all CSV lines before writing them
 
+$mediaTypes =['0db579ad-a810-45bb-acd1-002bf314b50f'  =>  'service',  #media use UUID taxonomy term for service files
+              '7bb44572-2b5c-4c28-b9f3-423c578455a8'  =>  'thumbnail',#media use UUID taxonomy term for thumbnail files
+              '5111671c-1010-4d63-9a53-5001aefc836a'  =>  'original'  #media use UUID taxonomy term for original files
+            ];
 
 #define function for grabbing attributes, using local variables so they reset with each get_called_class
 
@@ -68,60 +68,38 @@ foreach ($dcNodes as $node){
              'body'     =>    $node->attributes->body->value,
              'citation' =>    $node->attributes->field_citation->value,
              'lat'      =>    getlatlon($node)[0],
-             'lon'      =>    getlatlon($node)[1]
+             'lon'      =>    getlatlon($node)[1],
+             'thumbnail'=>    NULL,
+             'service'  =>    NULL,
+             'original' =>    NULL
 
   ];
-  echo $dcNode['body']."\n";
+  echo $dcNode['title']."\n";
 
-  $dcNode_digital_id=$node->attributes->field_digital_id;
-  $dcNode_uuid=$node->id; #get the uuid of the current dc node
-  $dcNode_url=$node->attributes->field_archival_resource_key->uri; #get the url of the current dc node
-  $dcNode_url=str_replace("http://n2t.net","http://special.library.unlv.edu",$dcNode_url); #change the hostname because the UNLV db stores a link to the EZID record
-  $dcNode_title=$node->attributes->title;
-  $dcNode_body=$node->attributes->body->value;
-  $dcNode_citation=$node->attributes->field_citation->value;
-  if (!empty($node->attributes->field_topic_location)) {
-    $dcNode_latitude=$node->attributes->field_topic_location[0]->lat;
-    $dcNode_longitude=$node->attributes->field_topic_location[0]->lng;
-  }
-  else {
-    $dcNode_latitude=NULL;
-    $dcNode_longitude=NULL;
-  }
   #iterate over mediaNodes for the current dc_node and find the associated files
   foreach ($mediaNodes as $mediaNode)
     {
-      $mediaParentID = $mediaNode->relationships->field_media_of->data->id; #this is the UUID of the dc node to which the current media node belongs
-      $mediaFileID = $mediaNode->relationships->field_media_image->data->id; #this is the file id of the associated file to the current media node
-      $mediaUse = $mediaNode->relationships->field_media_use->data[0]->id; #this is the UUID of the taxonomy term attached to media node SEE SET VARIABLES ABOVE
-      if ($mediaParentID==$dcNode_uuid&&$mediaUse==$mediaUse_taxonomy_uuid) {  #it looks like some don't have service files
+      $media = ['parent_id'   =>    $mediaNode->relationships->field_media_of->data->id,      #this is the UUID of the dc node to which the current media node belongs
+                'file_id'     =>    $mediaNode->relationships->field_media_image->data->id,   #file id of the associated file to the current media node
+                'usage'       =>    $mediaNode->relationships->field_media_use->data[0]->id   #this is the UUID of the taxonomy term attached to media node SEE SET VARIABLES ABOVE
+      ];
+
+      if ($media['parent_id']==$dcNode['uuid']) {  #it looks like some don't have service files
+        $type = $mediaTypes[$media['usage']];
+        echo $type;
         foreach ($fileNodes as $fileNode) {
           $fileId = $fileNode->id;
-          if ($fileId==$mediaFileID) {
+          if ($fileId==$media['file_id']) {
+            $dcNode[$type] = "http://special.library.unlv.edu".$fileNode->attributes->uri->url;
             $dcNode_fileURL = "http://special.library.unlv.edu".$fileNode->attributes->uri->url;
-            break;
           }
         }
-        break;
       }
     }
-  #if (empty($dcNode_parent)) {$dcNode_parent="NULL";} else {$dcNode_parent=$dcNode_parent[0]->id;}
 
-  #build the row of the CSV as an array
-  $rowArray=[$dcNode_digital_id,
-             $dcNode_uuid,
-             $dcNode_title,
-             $dcNode_url,
-             $dcNode_latitude,
-             $dcNode_longitude,
-             $dcNode_body,
-             $dcNode_fileURL,
-             $dcNode_citation];
-  #combine row elements into text using a delimiter
-  $rowText = implode("|",$rowArray)."\n";
-
-  #write the row to the csv file
-  fwrite($output,$rowText);
+  #write the row to the csv file using a delimiter
+  #fputcsv($output,$dcNode,'|');
+  array_push($csvLines,$dcNode);
 
   #only test on a handful during development
   if ($counter==10) { break;}
@@ -129,7 +107,12 @@ foreach ($dcNodes as $node){
 
 }
 
-#we've finished
+#we've finished, now time to output results
+$output = fopen("CSV-OUTPUT/import.csv", "w");  #open an a file to output as csv
+fputcsv($output,array_keys($csvLines[0]),'|'); #output headers to first line of CSV file
+foreach ($csvLines as $line) {
+  fputcsv($output,$line,'|');
+}
 fclose($output); #close the output file
 echo "END\n";    #let the user know we are done
 

fc327f50e42ed7855a34166828b5f6477621dde3
Chris Church <christopherchurch@unr.edu>
2021-07-06 16:52:00 -0700

    code now works but the input CSVs need to be well formed - have not dealt with children yet

--- METADATA-MERGE/03_append-local-metadata.php
+++ METADATA-MERGE/03_append-local-metadata.php
@@ -13,31 +13,56 @@ DATE LAST UPDATED
  07-06-2021
 */
 
-$input_path = "INPUT/UNR_metadata_2021-07-06.csv";
-$input_data = fopen($input_path,"r");
-$first_row = fgetcsv($input_data,0,$separator = "|");
-$csvKeys=[];
-$csvLines=[];
 
-$keys = array_values($first_row);
-  foreach ($keys as $index=>$key) {
-    if ($key==NULL) { $key = "BLANK".$index;}
-    $key = preg_replace("/[^A-Za-z0-9]/", '', $key);
-    $key = substr($key, 0, 20);
-    array_push($csvKeys, $key);
-  }
+#************************MAIN***************************
+$output_path = "OUTPUT/output.csv"; #where we will save the CSV
 
-#print_r($csvKeys);
+$UNR_metadata = fetchCSV("INPUT/UNR_metadata_2021-07-06.csv",'DigitalitemIDNNN'); #grab the data from UNR
+$UNLV_metadata = fetchCSV("../JSON-API/CSV-OUTPUT/import.csv",'did');             #grab the data from UNLV
 
-while (($row = fgetcsv($input_data,0,$separator = "|")) != FALSE) {
-    $csvLine=[];
-    foreach ($row as $index=>$r) {
-        $csvLine[$csvKeys[$index]]=$r;
+$combined = array_merge_recursive($UNLV_metadata,$UNR_metadata);                  #merge array on shared key 'did' vs 'DigitalitemIDNNN'
+
+makeCSV($output_path,$combined);  #export the combined array as a CSV
+
+#************************MFUNCTIONS**********************
+
+function fetchCSV($input_path,$_UID_KEY) {
+/*This function fetches as CSV at the $input_path, stores all the rows in an associative array with the key provided as an argument pulled from each entry*/
+  $input_data = fopen($input_path,"r");
+  $first_row = fgetcsv($input_data,0,$separator = "|");
+  $csvKeys=[];
+  $csvLines=[];
+
+  $keys = array_values($first_row);
+    foreach ($keys as $index=>$key) {
+      if ($key==NULL) { $key = "BLANK".$index;}
+      $key = preg_replace("/[^A-Za-z0-9]/", '', $key);
+      $key = substr($key, 0, 20);
+      array_push($csvKeys, $key);
     }
-    array_push($csvLines,$csvLine);
+
+  #print_r($csvKeys);
+
+  while (($row = fgetcsv($input_data,0,$separator = "|")) != FALSE) {
+      $csvLine=[];
+      foreach ($row as $index=>$r) {
+          $csvLine[$csvKeys[$index]]=$r;
+      }
+      $csvLines[$csvLine[$_UID_KEY]]=$csvLine;
+  }
+  fclose($input_data);
+  return$csvLines;
 }
 
-#print_r($csvLines);
+function makeCSV($_output_path,$finalNodeArray) {
+#This function exports the provided array as a CSV to the output path
+  $output = fopen($_output_path, "w");  #open an a file to output as csv
+  $initArrayKey=array_key_first($finalNodeArray);
+  fputcsv($output,array_keys($finalNodeArray[$initArrayKey]),'|'); #output headers to first line of CSV file
+  foreach ($finalNodeArray as $line) {
+    fputcsv($output,$line,'|');
+  }
+  fclose($output); #close the output file
+}
 
-fclose($input_data);
  ?>

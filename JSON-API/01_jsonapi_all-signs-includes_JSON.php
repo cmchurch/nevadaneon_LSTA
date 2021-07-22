fdeebd22f454f80771f35613b884a1b5207c9190
Chris Church <christopherchurch@unr.edu>
2021-07-22 13:53:08 -0700

    updated JSON-API script to grab via collection and material, so that we can specify the Dreaming the Skyline Materials from YESCO

--- JSON-API/01_jsonapi_all-signs-includes_JSON.php
+++ JSON-API/01_jsonapi_all-signs-includes_JSON.php
@@ -23,10 +23,14 @@ echo "**************************************************************************
 	#prefixes and collection identifiers -> first in associative array is prefix on the digital id and second is the UUID of the collection
 	#also available "sky" (did not include "sky" because the collection includes much more than NEON)
 	 $digital_id_prefixes = [
-                            "NNN"=>null, #note: as of 6-2021, the NNN materials have not been assigned a collection, so it is left null
-                            "pho"=>"1661de4d-e916-4108-b529-f3b15030c27b", #materials with prefix pho in the Neon in Nevada Photograph Collection (uuid provided)
-                            "neo"=>null #materials with prefix neo (neon signs), no specific collection
+                            "NNN"=>["collection"=>null,"material"=>null], #note: as of 6-2021, the NNN materials have not been assigned a collection, so it is left null
+                            "pho"=>["collection"=>"1661de4d-e916-4108-b529-f3b15030c27b","material"=>null], #materials with prefix pho in the Neon in Nevada Photograph Collection (uuid provided)
+                            "neo"=>["collection"=>null,"material"=>null], #materials with prefix neo (neon signs), no specific collection
+                            "sky"=>["collection"=>"8e8bfc18-73ed-4fad-8017-f56890c55219","material"=>"b8a468e1-1e3d-46b4-8ab6-f7f527b86e1e"]  #'sky' images from the YESCO CORPORATE COLLECTION and DREAKMING THE SKYLINE- material type: photographs (b8a468e1-1e3d-46b4-8ab6-f7f527b86e1e)
                           ];
+
+
+  #field_media_of.field_digital_id   &filter[field_media_of.field_material_type.id]=b8a468e1-1e3d-46b4-8ab6-f7f527b86e1e
 	#the URL of the ENDPOINT (dc_content) plus filters
 	 $crawl_url="http://special.library.unlv.edu/jsonapi/media/image/?include=field_media_image,field_media_of&fields[file--file]=uri,url&fields[node--node]=field_digital_id&filter[prefix][condition][path]=field_media_of.field_digital_id&filter[prefix][condition][operator]=STARTS_WITH&filter[prefix][condition][value]=";
 	#initialize an empty array to which we can append the data results from each call of the JSON API
@@ -36,7 +40,7 @@ echo "**************************************************************************
 
    #HIT JSON ENDPOINT WITH FILTER FOR EACH PREFIX IN "field_digital_id" OF NODE DC_CONTENT OBJECT, AS WELL AS THE COLLECTION IDENTIFIER IF RELEVANT
    #THIS WILL COLLECT BOTH PARENTS AND CHILDREN
-	foreach ($digital_id_prefixes as $prefix=>$collection) {
+	foreach ($digital_id_prefixes as $prefix=>$details) {
 		echo "\nGETTING ID PREFIX $prefix FROM COLLECTION IF SPECIFIED USING MEDIA OBJECT ENDPOINT AND INCLUDES \n";		#tell user we're working
 
 		#INIT VARIABLES LOCAL TO FOREACH LOOP
@@ -45,8 +49,9 @@ echo "**************************************************************************
 
 		while (sizeof($tempData)>0) {	#continue looping as long as the JSON API does not return an empty data element in the results per offset
 			$url=$crawl_url.$prefix."&page[limit]=50&page[offset]=".$offset; #built JSON API endpoint URL using prefix information
-			if (isset($collection)) $url=$url."&filter[field_media_of.field_archival_collection.id]=".$collection; #add the collection UUID to URL filter if present in associative array
-			$data=file_get_contents($url); #GET the JSON API results from the ENDPOINT AS A JSON OBJECT
+			if (isset($details['collection'])) $url=$url."&filter[field_media_of.field_archival_collection.id]=".$details['collection']; #add the collection UUID to URL filter if present in associative array
+	    if (isset($details['material'])) $url=$url."&filter[field_media_of.field_material_type.id]=".$details['material'];           #if a material is specified, then limit search results to that material
+      $data=file_get_contents($url); #GET the JSON API results from the ENDPOINT AS A JSON OBJECT
 			$jsonObj=json_decode($data);
 			$tempData=$jsonObj->data; #READ THE DATA ELEMENT INTO PHP FROM THE DECODED JSON OBJECT -> for this script, these are the media objects
 			if (sizeof($tempData)>0) {
